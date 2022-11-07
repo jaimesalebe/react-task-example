ee1546cbafc685ac1d25fed83e754ae49c7b74e0
Jaime Salebe <jaimesalebe@outlook.com>
2022-11-06 22:26:16 -0500

    first commit

---
+++ src/data/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: "Mi primer tarea",
+    description: "Tarea #1",
+  },
+  {
+    id: 1,
+    title: "Mi segunda tarea",
+    description: "Tarea #2",
+  },
+  {
+    id: 2,
+    title: "Mi tercer tarea",
+    description: "Tarea #3",
+  },
+];
